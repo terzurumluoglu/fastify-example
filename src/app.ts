98aaf4fba48ecc0f5d5adbed678a147552cb609b
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2025-01-08 16:20:14 +0300

    root response added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { join } from "path";
 import { Server } from "./server/server";
 import fastifyAutoload from "@fastify/autoload";
+import { type FastifyReply, type FastifyRequest } from "fastify";
+import { HTML } from "@constants";
+// import { createReadStream } from "fs";
 
 const { server } = Server.get();
 
@@ -10,6 +13,14 @@ const register = () => {
 };
 
 const registerPlugin = async () => {
+  server.get("/", (req: FastifyRequest, reply: FastifyReply) => {
+    // try {
+    //   const path = join(__dirname, "index.html");
+    //   const stream = createReadStream(path);
+    //   reply.type("text/html").send(stream);
+    // } catch (error) {}
+    reply.type("text/html").send(HTML);
+  });
   const dir = join(__dirname, "plugins");
   server.register(fastifyAutoload, {
     dir,
