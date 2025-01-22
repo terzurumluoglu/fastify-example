fb16ddc1959de7d3a38c5d455cacc2dd81ad3e68
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2025-01-22 20:50:04 +0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { join } from "path";
 import { Server } from "./server/server";
 import fastifyAutoload from "@fastify/autoload";
 import { errorHandler } from "helpers";
+import cors from "@fastify/cors";
 
 const { server } = Server.get();
 
@@ -9,6 +10,11 @@ const register = async () => {
   // Add other registers
   // ...
   server.setErrorHandler(errorHandler);
+
+  server.register(cors, {
+    methods: "*",
+    origin: "*",
+  });
 
   const dir = join(__dirname, "plugins");
   server.register(fastifyAutoload, {
